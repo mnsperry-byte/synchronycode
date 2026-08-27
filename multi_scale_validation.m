f42dcfd7da10ef825af270b7b4f28fa7a3ad7731
mnsperry-byte <mnsperry@ucsc.edu>
2026-08-27 15:36:15 -0700

    Add files via upload

---
+++ multi_scale_validation.m
@@ -0,0 +1,245 @@
+
+%================= global parameters ======================%
+
+L = 15000; % half domain length
+N = 100000; % Number of points for discretization
+
+x = linspace(-L,L-(2*L/N),N); % grid
+dx = x(2)-x(1); %caldulate grid size
+
+numsteps = 1000; %number of steps for propogation
+
+% Dispersal kernel (Gaussian)
+alpha = 4;
+k = exp(-x.^2/(2*alpha^2));
+k = k/sum(k);
+
+
+% Environmental driver correlation structures
+alpha1 = 0.5; 
+alpha2 = 0.5;
+
+sigma1 = 1e-3;
+sigma2 = 1e-3;
+
+lambda1 = 1;
+phi = 1;
+
+g1 = exp(-x.^2/(2*(lambda1/sqrt(2))^2));
+g1 = g1/sqrt(sum(g1.^2));
+
+g2 = exp(-phi*abs(x));
+g2 = g2/sqrt(sum(g2.^2));
+
+
+% ================ simulation infrastructure ==================%
+
+%Regimes
+bounds = [ ...
+    0.001, ...
+    2.0000, ...
+    2.5265, ...
+    2.6564 ...
+]; %These are predetermined bounds for periods 1-2-4 regimes
+
+rvals = [];
+for i = 1:length(bounds)-1
+    regime_vals = linspace(bounds(i),bounds(i+1),100);
+    rvals = [rvals regime_vals];
+end   % 100 simulations for each regime
+fprintf('Total R values = %d\n',length(rvals))
+
+% main loop storage 
+lTheory = cell(size(rvals)); %space for theoretical prediction
+lEmp = cell(size(rvals)); %space for simulation result 
+period_store = nan(size(rvals)); %space for assigning an empircally inferred period to each simulation
+
+
+% ===================== main loop =========================
+
+%shell 1: loop through r vals
+for ir = 1:length(rvals)
+    r = rvals(ir);
+    fprintf('\nRunning r = %.4f\n',r)
+    
+    % shell 2: simulate rval dynamics
+    %initialization
+    n = zeros(numsteps+1,N);
+    n(1,:) = 1.1; 
+
+    for t = 1:numsteps
+
+        %generate random noise each step
+        b1 = randn(1,N); 
+        b2 = randn(1,N);
+
+        %apply correlation structure
+        z1 = sigma1*real(ifft(fft(g1).*fft(b1)));
+        z2 = sigma2*real(ifft(fft(g2).*fft(b2)));
+        
+        %define forcing
+        forcing = n(t,:).*exp(r*(1-n(t,:)) ...
+                  + alpha1*z1 + alpha2*z2);
+
+        % convolve with dispersal to propogate
+        n(t+1,:) = real(ifft(fft(k).*fft(forcing)));
+    end
+
+
+    % shell 3: simulation post processing, empirical part:
+    % step 1: find and report determinsitic orbit for phase alignment
+
+    Nval = 1.2;
+    for i = 1:10000
+        Nval = Nval*exp(r*(1-Nval));
+    end
+    maxPeriod = 16;
+    orbit = zeros(maxPeriod+1,1);
+    orbit(1)=Nval;
+    
+    for i=1:maxPeriod
+        orbit(i+1)=orbit(i)*exp(r*(1-orbit(i)));
+    end
+    period=[];
+    for p=1:maxPeriod
+        if abs(orbit(end)-orbit(end-p)) < 1e-12
+            period=p;
+            break
+        end
+    end
+    if isempty(period)
+        fprintf('No periodic orbit\n')
+        continue
+    end
+
+    %assign empircally inferred deterministic orbit period
+    period_store(ir)=period;
+    fprintf('Period = %d\n',period)
+
+
+    % phase alignment order
+    Nstar = orbit(end-period+1:end);
+
+    % step 2: extract empircal length scales for each reference phase in
+    % the cycle
+    spatial_means=mean(n,2); %assumption: spatial mean should be close to determinstic attractor 
+    search_range = 400:(numsteps-period);
+
+    % first phase alignment
+    [~,idx]=min(abs(spatial_means(search_range)-Nstar(1)));
+    t0=search_range(idx);
+    % Check alignment is close
+    phase_error=zeros(period,1);
+    for phase=1:period
+        phase_error(phase)=abs(spatial_means(t0+phase-1)-Nstar(phase));
+    end
+    if max(phase_error)>1e-3
+        fprintf('Bad phase alignment\n')
+        continue
+    end
+    
+    % for phase in the period calculate a seperate empircal correlation
+    % function
+    lEmp_phase=zeros(period,1);
+
+    for phase=1:period
+        n_dec=n(t0+phase-1:period:end,:);
+        maxdist=100;
+        maxlag=round(maxdist/dx);
+        s=(0:maxlag)*dx;
+        
+        %correlation function
+        frames=size(n_dec,1);
+        C=zeros(1,maxlag+1);
+        for t=1:frames
+            data=n_dec(t,:)-mean(n_dec(t,:)); %subtract from mean
+            for h=0:maxlag
+                C(h+1)=C(h+1)+mean(data.*circshift(data,-h)); %two point periodicmnkk correlation
+            end
+        end
+        C=C/frames;
+
+        %truncation
+        tail=C(round(0.8*length(C)):end);
+        threshold=3*std(tail);
+        window=5;
+        idx_cut=length(C);
+        for i=1:length(C)-window+1
+            if all(abs(C(i:i+window-1))<threshold)
+                idx_cut=i;
+                break
+            end
+        end
+
+        %calculate length scale
+        l2=sum((s(1:idx_cut).^2).*C(1:idx_cut)) ...
+           /sum(C(1:idx_cut));
+
+        lEmp_phase(phase)=sqrt(abs(l2));
+    end
+
+    %Save all the length scales to corresponding r value
+    lEmp{ir}=lEmp_phase;
+
+
+    % shell 3: simulation post processing, analytical part (prediction)
+    
+    % derivative definitions
+    fN=@(Nin) exp(r*(1-Nin)).*(1-r*Nin);
+    fi=@(a,Nin) a.*Nin.*exp(r*(1-Nin));
+
+    lTheory_phase=zeros(period,1);
+
+    %Make theoretical predictions for each possible phase cycle
+    for phase=1:period
+        Nphase=circshift(Nstar,-(phase-1));
+       
+        %calculate relevant Floquet multipliers
+        F=prod(fN(Nphase));
+        A=zeros(period,1);
+        for i=1:period
+            if i==period
+                A(i)=1;
+            else
+                A(i)=prod(fN(Nphase(i+1:end)));
+            end
+        end
+
+        % integrated correlation structures
+        P01=sum(sigma1^2*exp(-x.^2/(2*lambda1^2)))*dx;
+        P02=sum(sigma2^2*exp(-phi*abs(x)))*dx;
+
+        %Environmental sensitivities squared
+        F1_sq=fi(alpha1,Nphase).^2;
+        F2_sq=fi(alpha2,Nphase).^2;
+
+        %Weights for each driver
+        Num1=A.^2 .* F1_sq * P01;
+        Num2=A.^2 .* F2_sq * P02;
+
+        Num=[Num1 Num2];
+        w=Num/sum(Num(:));
+
+        % dispersal term
+        mu_grid=(0:period-1)';
+        coeff_matrix=2*(period-mu_grid).*w;
+        dispersal_coeff=sum(coeff_matrix(:));
+
+
+        dispersal_term=((2*period*F^2)/(1-F^2) ...
+                       +dispersal_coeff)*alpha^2;
+
+        % environmental term
+        environmental_term=sum(w(:,1)*lambda1^2 ...
+                             +w(:,2)*(2/phi)^2);
+
+        % calculate length scale
+        lu2=dispersal_term+environmental_term;
+        lTheory_phase(phase)=sqrt(abs(lu2));
+    end
+
+    %save all length scales to corresponding r value
+    lTheory{ir}=lTheory_phase;
+end
+
+%Plotting Lemp and Ltheory on the same map gives the validation plot.
